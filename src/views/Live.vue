e52666876339390ade7e81b4dab959433a7ed642
陳昱安 <ann@chenyuandeMacBook-Pro.local>
2023-02-10 12:34:18 +0800

    add live page & demo p5.js script

---
+++ src/views/Live.vue
@@ -0,0 +1,27 @@
+<template lang="pug">
+.page.about.text-left
+  .container
+    .row
+      .col-md-7
+        h1.animated.fadeInUp 
+          span Live
+          br
+          span.strokeText TRY ON
+    .row
+      .col-12
+        iframe#liveham.w-100(
+          width="100%",
+          height="800px",
+          src="/liveham/index.html",
+          :key="$store.state.width"
+        )
+</template>
+<script>
+export default {
+  mounted() {
+    let elHam = document.querySelector("#liveham");
+    let ww = elHam.offsetWidth;
+    elHam.setAttribute("height", ww + "px");
+  },
+};
+</script>
